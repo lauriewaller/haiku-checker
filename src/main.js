d738b74762c444f6093ef69f3dbd54e07ccb9039
Garrett Brown <garrettpaulbrown@gmail.com>
2021-07-25 16:39:14 -0700

    More file changes.
    Co-authored-by: Laurie Waller <lauriewaller7@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+const countVowels = str => Array.from(str)
+  .filter(letter => 'aeiou'.includes(letter)).length;
+
+
